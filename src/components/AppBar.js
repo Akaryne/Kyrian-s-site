243dffb3122f9dda739dd312cb7d0215d2052135
PENILLON Kyrian <k0penill@enib.fr>
2023-08-02 10:18:32 +0200

    login dialog

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -5,7 +5,8 @@ import './AppBar.css'
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 import DehazeOutlinedIcon from '@mui/icons-material/DehazeOutlined';
-import { Button, Icon, Menu, MenuItem } from "@mui/material";
+import { Button, Icon, Menu, MenuItem, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Paper,TableContainer, Typography,TextField } from "@mui/material";
+
 
 
 
@@ -15,6 +16,8 @@ function AppBar() {
 
     const [anchorElInformation, setAnchorElInformation] = useState(null);
     const [redirectToHome, setRedirectToHome] = useState(false)
+    const [openLoginDialog, setOpenLoginDialog] = useState(false)
+    const [openSignInDialog, setOpenSignInDialog] = useState(false)
     const open = Boolean(anchorElInformation);
 
     const handleClickInformation = (event) => {
@@ -23,6 +26,14 @@ function AppBar() {
     const handleCloseInformation = () => {
         setAnchorElInformation(null);
     };
+
+    const handleLoginDialog = () => {
+        setOpenLoginDialog(true)
+    }
+
+    const handleSignInDialog = () => {
+        setOpenSignInDialog(true)
+    }
 
     return(
             <div className='container-header'>
@@ -62,11 +73,191 @@ function AppBar() {
                     >
                         <MenuItem onClick={handleCloseInformation}>Contact</MenuItem>
                         <MenuItem onClick={handleCloseInformation}>A propos</MenuItem>
+                        <MenuItem onClick={handleLoginDialog}>Login</MenuItem>
+                        <MenuItem onClick={handleSignInDialog}>Sign in</MenuItem>
                     </Menu>
                 </div>
+               
+                <LoginDialog open={openLoginDialog} onQuit={()=>setOpenLoginDialog(false)}></LoginDialog>
+                <SignInDialog open={openSignInDialog} onQuit={()=>setOpenSignInDialog(false)}></SignInDialog>
+                
                 {redirectToHome && (
                     <Navigate to="/" replace={true} />
                 )}
             </div>
     )
 }export default AppBar
+
+const LoginDialog = ({ open, onQuit }) => {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(false)
+
+    
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
+      // Construire l'objet de données à envoyer dans la requête POST
+      const dataToSend = {
+        login: login,
+        password: password,
+      };
+  
+
+      // Effectuer la requête POST vers l'endpoint /add
+      fetch('https://game-zone-f7b9ede0718d.herokuapp.com/subscribe', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataToSend),
+      })
+        .then((response) => response.json())
+        .then(() => {
+            setError(false)
+        })
+        .catch((error) => {
+          console.error('Erreur lors de la requête POST :', error);
+          setError(true)
+          // Gérer les erreurs éventuelles ici
+        });
+
+    };
+  
+    return (
+      <Dialog open={open} onClose={onQuit}>
+        <DialogTitle style={{
+          color: '#7b0000',
+          textShadow: '#0000004d 3px 3px 3px',
+          fontFamily: 'Inter',
+          fontSize: '50px',
+          fonStyle: 'normal',
+          fontWeight: '600',
+          letterSpacing:' 4px',
+      }}>Login</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <Paper elevation={3}>
+            <TextField
+                required
+                id="outlined-required"
+                label="Pseudo"
+                placeholder="Pseudo"
+                onChange={(e) => setLogin(e.target.value)}
+            />
+            <TextField
+                required
+                id="outlined-required"
+                label="Password"
+                placeholder="password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            </Paper>
+            {error ? <Typography sx={{
+                textAlign: 'center',
+                color: 'brown',
+                mt: 1,}}>Error login or password incorrect</Typography> : null}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions style={{justifyContent:'space-around'}}>
+          
+          <Button onClick={handleSubmit} style={{
+            color: 'white',
+            background: 'rgb(82, 202, 0)',
+            fontSize: '16px',
+            height:'100%'
+          }}>
+            Login
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+
+  const SignInDialog = ({ open, onQuit }) => {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(false)
+    
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
+      // Construire l'objet de données à envoyer dans la requête POST
+      const dataToSend = {
+        login: login,
+        password: password,
+      };
+  
+
+      // Effectuer la requête POST vers l'endpoint /add
+      fetch('https://game-zone-f7b9ede0718d.herokuapp.com/subscribe', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataToSend),
+      })
+        .then((response) => response.json())
+        .then(() => {
+            setError(false)
+            onQuit()
+        })
+        .catch((error) => {
+          console.error('Erreur lors de la requête POST :', error);
+          setError(true)
+          // Gérer les erreurs éventuelles ici
+        });
+
+    };
+  
+    return (
+      <Dialog open={open} onClose={onQuit}>
+        <DialogTitle style={{
+          color: '#7b0000',
+          textShadow: '#0000004d 3px 3px 3px',
+          fontFamily: 'Inter',
+          fontSize: '50px',
+          fonStyle: 'normal',
+          fontWeight: '600',
+          letterSpacing:' 4px',
+      }}>Sign in</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <Paper elevation={3}>
+            <TextField
+                required
+                id="outlined-required"
+                label="Pseudo"
+                placeholder="pseudo"
+                onChange={(e) => setLogin(e.target.value)}
+            />
+            <TextField
+                required
+                id="outlined-required"
+                label="Password"
+                placeholder="password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            </Paper>
+            {error ? <Typography sx={{
+                textAlign: 'center',
+                color: 'brown',
+                mt: 1,}}>Pseudo are already used</Typography> : null}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions style={{justifyContent:'space-around'}}>
+          
+          <Button onClick={handleSubmit} style={{
+            color: 'white',
+            background: 'rgb(82, 202, 0)',
+            fontSize: '16px',
+            height:'100%'
+          }}>
+            Login
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
